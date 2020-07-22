62515b53d18f17de7427a02ca9f92aff1ca08080
spwebler <66193376+spwebler@users.noreply.github.com>
2020-07-22 19:27:25 +0530

    Day3

---
+++ Day3/assig1.py
@@ -0,0 +1,11 @@
+num = int(input("enter no"))
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
