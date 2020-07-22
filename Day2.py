62d70dab772dafbf29a18cbd9141a87d682f09c1
spwebler <66193376+spwebler@users.noreply.github.com>
2020-07-22 09:12:51 +0530

    Day2

---
+++ Day2.py
@@ -0,0 +1,77 @@
+print("Hello")
+print("Hello\Python")
+print(""" _____$$$$_________$$$$
+___$$$$$$$$_____$$$$$$$$
+_$$$$$$$$$$$$_$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$
+_$$$$$$$$$$$$$$$$$$$$$$$$$
+__$$$$$$$$$$$$$$$$$$$$$$$
+____$$$$$$$$$$$$$$$$$$$
+_______$$$$$$$$$$$$$
+__________$$$$$$$
+____________$$$
+_____________$
+ """)
+
+print(""" this program """)
+print('Hellow World')
+print("Python's worls")
+print('python\'s world')
+print("hello\a")
+
+name="shraddha"
+marks="50"
+age="20"
+print("The name of person is",name,"marks is",marks,"age is",age)
+
+print("The name of peraon is %10s marks is %f age is %d"%(name,age,marks))
+print(f"The name of person is{name} and the marks is{90.867}age is {age}")
+
+cisco_temp=20
+x=y=z=10
+x
+x=20
+id(x)
+
+
+del x
+x
+y=20
+id(y)
+
+5+4
+10/2
+10%3
+
+20//3
+
+a=10
+b=20
+a==b
+
+c=10
+a==c
+
+c<=10
+
+a!=10
+
+a=240
+b=1
+a|b
+bin(a)
+
+
+a>20 or 10>9
+
+10>7 and m>4
+
+a=-4
+b=-4
+a in b
+
+10+10/29*4
+
+
+
