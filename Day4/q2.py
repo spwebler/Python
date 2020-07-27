3467eb03d9d932f9ecea7fe96147480260eea191
spwebler <66193376+spwebler@users.noreply.github.com>
2020-07-27 17:08:40 +0530

    Update q2.py

--- Day4/q2.py
+++ Day4/q2.py
@@ -6,13 +6,23 @@ txt = "CAPITALIZEEEE!"
 
 x = txt.isupper()
 x2=txt.islower()
+x3="letsupgrade"
+x3.isupper()
+x4="LetsUpgrade1"
+x4.isupper()
 print(x2)
 
 print(x)
-
+str1="Letsupgrade"
+str1.isupper()
 #The islower() method returns True if all the characters are in lower case, otherwise False.
 txt1 = "i am shorterr"
 
 x1 = txt1.islower()
+
+x3="LetsUpgrade1"
+x3.islower()
+x4="LETSUPGRADE"
+x5.islower()
 
 print(x1)
