f1c1e80efe8fbf6b139885d60ef530cd6637844c
Shraddha Prabhu <66193376+spwebler@users.noreply.github.com>
2022-06-11 23:04:42 +0530

    Update practiselist.py

--- practiselist.py
+++ practiselist.py
@@ -5,3 +5,16 @@ l.pop(5)
 print(l)
 l.insert(2,"shra")
 print(l)
+
+
+//store application
+print("Welcome to Store")
+print("Choose any three option from our store:\n 1:Pizza 2:Sandwitch 3:Toast 4:Ran")
+n=0
+while(n<3):
+    print("Your"+str(n+1)+"order",int(input()))
+    n=n+1
+print("How many you want")
+count=int(input())
+
+print("Your purchase:",count*5)
